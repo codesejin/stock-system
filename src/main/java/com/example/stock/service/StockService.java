e5a1de85520a924c2cb7d8e153e32c9d54bc0d2c
sejin park <seijin0722@naver.com>
2023-11-22 18:31:08 +0900

    @Transactional 트랜잭션 추가

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,6 +14,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소시킨 뒤
