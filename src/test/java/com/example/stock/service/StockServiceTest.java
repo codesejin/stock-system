8e93bc167bb8874b7b29c6bef3a076ec6752461c
sejin park <seijin0722@naver.com>
2023-11-22 18:25:06 +0900

    문제점 : 요청이 동시에 여러개 들어올 경우, 예상과 다른 재고 수량 race contidion : 2개 이상의 스레드가 공유 데이터에 액세스 할 수 있고 동시에 변경하려고 할때 발생

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -37,5 +41,28 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99,stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        // 멀티스레드 사용 : ExecutorService는 비동기로 실행하는 작업을 단순화해서 사용할 수 있는 Java API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 100개의 요청이 모두 끝날때까지 기다려야함 : CountDownLatch는 다른 Thread에서 수행 중인 작업 완료될때까지 대기
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
     }
 }
